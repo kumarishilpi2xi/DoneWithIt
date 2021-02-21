46f463979ce330ec598fb2a2af39b90b62b76bac
kumari-shilpi <kumari.shilpi@mantralabsglobal.com>
2021-02-21 20:53:04 +0530

    project setup, welcome screen ui

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Image, ImageBackground, StyleSheet, View } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/niko-2.jpg")}
+    >
+        <Image style={styles.logo} source={require("../assets/store.png")} />
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  logo:{
+      width:100,
+      height:100,
+      position:"absolute",
+      top:70,
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
+export default WelcomeScreen;
